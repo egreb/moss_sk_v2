23c91e3eaf2c821c257a7c15736da2ccd1409ce1
Simen Berge <bergesimen@gmail.com>
2019-11-17 20:55:57 +0100

    add some shortcuts to creating new items on admin dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,31 +1,33 @@
 @extends('layouts.admin')
 
 @section('content')
-    <section class="p-2 w-full flex flex-col justify-center">
+    <section class="p-2 w-full flex flex-col" style="align-items: flex-start;">
         <header class="flex justify-between md:w-10/12 lg:w-8/12 items-center">
-            <a href="{{ route('admin.post.index') }}">
-                <h2 class="text-3xl">Nyheter</h2>
-            </a>
+            <h2 class="text-3xl">Nyheter</h2>
         </header>
         @if(isset($posts) && !$posts->isEmpty())
-            <ul class="flex flex-col">
+            <ul class="flex flex-col w-full">
                 @foreach($posts as $post)
                     <li class="flex justify-between border mt-2 p-2 md:w-10/12 lg:w-8/12">
                         <a href="{{ route('admin.post.edit', ['id' => $post->id]) }}"><h3>{{ $post->title }}</h3></a>
                         <span class="text-gray-500">Oppdatert {{ $post->updated_at->format('h:t d-m-y') }}</span>
                     </li>
                 @endforeach
-
             </ul>
+
+            <footer class="mt-3">
+                <a href="{{ route('admin.post.create') }}" class="btn btn-success">Ny</a>
+                <a href="{{ route('admin.post.index') }}" class="btn btn-blue mt-3">Se alle</a>
+            </footer>
         @else
             <p class="text-blue-500">Det er ikke opprettet noen nyheter.</p>
         @endif
     </section>
 
-    <section class="p-2 w-full flex flex-col justify-center">
+    <section class="p-2 w-full flex flex-col justify-center items-start">
         <h2 class="text-3xl">Terminlister</h2>
         @if(isset($posts))
-            <ul class="flex flex-col">
+            <ul class="flex flex-col w-full">
                 @foreach($schedules as $schedule)
                     <a class="flex justify-between border mt-2 p-2 md:w-10/12 lg:w-8/12"
                        href="{{ route('admin.schedule.edit', ['id' => $schedule->id]) }}">
@@ -36,15 +38,19 @@
                         @endif
                     </a>
                 @endforeach
-
             </ul>
+
+            <footer class="mt-3">
+                <a href="{{ route('admin.schedule.create') }}" class="btn btn-success">Ny</a>
+                <a href="{{ route('admin.schedule.index') }}" class="btn btn-blue mt-3">Se alle</a>
+            </footer>
         @endif
     </section>
 
-    <section class="p-2 w-full flex flex-col justify-center">
+    <section class="p-2 w-full flex flex-col justify-center items-start">
         <h2 class="text-3xl">Medlemmer</h2>
         @if(isset($members) && !$members->isEmpty())
-            <ul class="flex flex-col">
+            <ul class="flex flex-col w-full">
                 @foreach($members as $member)
                     <a class="flex justify-between border mt-2 p-2 md:w-10/12 lg:w-8/12"
                        href="{{ route('admin.schedule.edit', ['id' => $member->id]) }}">
@@ -52,6 +58,11 @@
                     </a>
                 @endforeach
             </ul>
+
+            <footer class="mt-3">
+                <a href="{{ route('admin.member.create') }}" class="btn btn-success">Ny</a>
+                <a href="{{ route('admin.member.index') }}" class="btn btn-blue mt-3"><small>Se alle</small></a>
+            </footer>
         @else
             <p class="text-gray-500">Det er ikke opprettet noen medlemmer enda</p>
         @endif
