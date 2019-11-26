de6b60586e0c928281f59c0211fe6a3570caa9b2
Simen Berge <bergesimen@gmail.com>
2019-11-26 22:36:19 +0100

    add upload image on editing post

--- resources/views/admin/post/shared/image.blade.php
+++ resources/views/admin/post/shared/image.blade.php
@@ -1,7 +1,9 @@
 <div class="mb-6 flex justify-center edit-post-image">
     @if(isset($image) && !is_null($image))
-        <div class="flex flex-col post-image">
-            <img src="{{ $image->url }}" />
+        <div class="flex flex-col post-image items-center">
+            <div class="w-6/12">
+                <img src="{{ $image->url }}" />
+            </div>
             <button name="delete_image" value="delete"
                     class="btn btn-danger mt-1">Slett bilde
             </button>
@@ -14,7 +16,7 @@
                     d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z"/>
             </svg>
             <span class="mt-2 text-base leading-normal">Last opp bilde</span>
-            <input type='file' name="image" id="image" class="hidden"/>
+            <input type='file' name="main_image" id="main_image" class="hidden"/>
         </label>
     @endif
 </div>
--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('image', 'ImageController@fetch');
+Route::post('image', 'ImageController@store');
