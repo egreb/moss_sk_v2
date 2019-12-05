6f3f8cebdaaab64ad2dcbafeebf2031f6a8217f3
Simen Berge <bergesimen@gmail.com>
2019-12-05 21:52:51 +0100

    full width on articles section frontpage

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -5,7 +5,7 @@
         @include('app.shared.hero')
 
         <div class="flex justify-center w-full bg-white rounded shadow">
-            <section class="articles p-2 flex flex-col justify-center items-center ">
+            <section class="articles p-2 flex flex-col justify-center items-center w-full">
                 <h2 class="text-gray-800 text-4xl text-center mb-6 border-b w-64">Nyheter</h2>
                 @foreach($posts as $post)
                     @include('app.shared.post', ['post' => $post])
