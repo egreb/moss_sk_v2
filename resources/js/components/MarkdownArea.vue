337289ae808eebbf3963743600531e919c14a72f
Simen Berge <bergesimen@gmail.com>
2020-01-15 21:57:45 +0100

    replace word at cursor with link

--- resources/js/components/MarkdownArea.vue
+++ resources/js/components/MarkdownArea.vue
@@ -26,8 +26,14 @@
             return {
                 text: '',
                 url: '',
-                el: null,
-                showModal: false
+                editor: null,
+                showModal: false,
+                position: {
+                    A1: null,
+                    B1: null,
+                    A2: null,
+                    B2: null
+                }
             }
         },
         props: {
@@ -45,46 +51,62 @@
             if (this.uploadImage) {
                 toolbar = [...toolbar, ...['|', 'upload-image']];
             }
-            toolbar = [...toolbar, {
-                name: 'link',
-                action: this.toggleLinkModal,
-                className: 'fa fa-link',
-                title: 'Link'
-            }];
-            // {
-            //     name: "gallery",
-            //     action: toggleGallery,
-            //     className: "fa fa-image",
-            //     title: "Vis Galleri",
-            // },
+            toolbar = [...toolbar,
+                {
+                    name: 'link',
+                    action: this.toggleLinkModal,
+                    className: 'fa fa-link',
+                    title: 'Link'
+                }];
 
-            this.el = new EasyMDE({
+            this.editor = new EasyMDE({
                 element: target,
                 initialValue: this.value,
                 uploadImage: this.uploadImage,
                 imageUploadEndpoint: '/api/image',
                 ...markdownConfig,
                 toolbar
-            })
-            ;
+            });
         },
         methods: {
-            toggleLinkModal() {
+            toggleLinkModal(e) {
+                const editor = e.codemirror.getDoc();
+                const A1 = editor.getCursor().line;
+                const A2 = editor.getCursor().ch;
+
+                const B1 = e.codemirror.findWordAt({line: A1, ch: A2}).anchor.ch;
+                const B2 = e.codemirror.findWordAt({line: A1, ch: A2}).head.ch;
+
+                const word = editor.getRange({line: A1, ch: B1}, {line: A1, ch: B2});
+                if (word) {
+                    this.position = {
+                        A1, B1, A2, B2
+                    };
+                    this.text = word;
+                } else {
+                    this.position = null;
+                }
+
                 this.showModal = true
             },
             insertLink() {
                 const link = `[${this.text}](${this.url})`;
 
-                const doc = this.el.codemirror.getDoc();
+                const doc = this.editor.codemirror.getDoc();
                 if (doc) {
                     const cursor = doc.getCursor();
-                    doc.replaceRange(link, cursor);
+
+                    if (this.position) {
+                        const {A1, B1, B2} = this.position;
+                        doc.replaceRange(link, {line: A1, ch: B1}, {line: A1, ch: B2})
+                    } else {
+                        doc.replaceRange(link, cursor);
+                    }
                 }
 
                 this.showModal = false
             },
             closeModal() {
-                console.log('check')
                 this.showModal = false;
             }
         },
--- resources/js/components/Modal.vue
+++ resources/js/components/Modal.vue
@@ -1,6 +1,6 @@
 <template>
     <div v-if="show" class="modal fixed w-full h-full top-0 left-0 flex items-center justify-center z-50"
-          id="modal">
+         id="modal">
         <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
 
         <div class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
@@ -61,9 +61,8 @@
             close: Function
         },
         watch: {
-           show: function(oldValue, newValue) {
-               console.log('watch', oldValue, newValue)
-               document.querySelector('body').classList.toggle('modal-active')
+            show: function (oldValue, newValue) {
+                document.querySelector('body').classList.toggle('modal-active')
             }
         },
         name: "Modal",
