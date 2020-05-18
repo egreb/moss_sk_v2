5d058e5d609ec301758c41d1ac40ba0e6be25b61
Simen Berge <bergesimen@gmail.com>
2020-05-18 23:32:25 +0200

    check if published_at isn't null

--- resources/views/admin/post/index.blade.php
+++ resources/views/admin/post/index.blade.php
@@ -14,7 +14,8 @@
                 <div class="w-full flex flex-col justify-center px-1">
                     <div class="flex items-center justify-between">
                         <div class="flex flex-col">
-                            <a href="{{ route('admin.post.edit', ['id' => $post->id]) }}" class="text-2xl">{{ $post->title }}</a>
+                            <a href="{{ route('admin.post.edit', ['id' => $post->id]) }}"
+                               class="text-2xl">{{ $post->title }}</a>
                             <div class="text-gray-500">
                                 Publisert av {{ $post->author_string() }}
                             </div>
@@ -25,7 +26,9 @@
                                 <div class="text-red-500 border-red-500 border py-1 px-2 text-center">Upublisert</div>
                             @else
                                 <span class="text-blue-500 border border-blue-500 py-1 px-2 rounded">Publisert
-                                    <time>{{ $post->published_at->format('d-m-y') }}</time>
+                                    @if (!is_null($post->published_at))
+                                        <time>{{ $post->published_at->format('d-m-y') }}</time>
+                                    @endif
                                 </span>
                             @endif
                         </div>
