185ad2a6774b29ff5a56a7b17018285173c455d3
Simen Berge <bergesimen@gmail.com>
2022-01-19 22:31:57 +0100

    fix overflow on small devices

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -3,10 +3,10 @@
 @section('content')
     @include('app.shared.hero')
 
-    <div class="flex justify-center w-full">
+    <div class="max-w-xl mx-auto">
         <section class="articles p-0 sm:p-2 flex flex-col justify-center items-center w-full">
             <h2 class="text-gray-800 text-4xl text-center border-b w-64 pb-4 pt-6">Nyheter</h2>
-            <section class="flex flex-col space-y-2 px-1">
+            <section class="flex flex-col space-y-4 px-1">
                 @foreach($posts as $post)
                     @include('app.shared.post', ['post' => $post])
                 @endforeach
